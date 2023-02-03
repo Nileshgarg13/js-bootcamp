1dc207da9bd05044af1d53c9062a8925a83b6213
Nileshgarg13 <ng5787@srmist.edu.in>
2023-02-04 00:57:52 +0530

    basic functions

--- hangman/app.js
+++ hangman/app.js
@@ -4,17 +4,22 @@ const guessesEl=document.querySelector("#guesses")
 const statusEl=document.querySelector("#status")
 const game1=new Hangman('cat',3)
 puzzleEl.textContent=game1.getPuzzle()
-guessesEl.textContent=game1.remainingGuesses
+// guessesEl.textContent=game1.remainingGuesses
 statusEl.textContent=game1.staus
 window.addEventListener('keypress',function(e)
 {
     const guess=e.key
     game1.makeguess(guess)
     puzzleEl.textContent=game1.getPuzzle()
-    guessesEl.textContent=game1.remainingGuesses
+    // guessesEl.textContent=game1.remainingGuesses
     statusEl.textContent=game1.staus
     if(game1.staus==='FAILED')
     {
-        statusEl.textContent=`FAILED THE WORD WAS ${game1.word}`
+        statusEl.textContent=game1.statusMessage()
     }
+    else if (game1.staus==='PLAYING')
+    {
+        statusEl.textContent=game1.statusMessage()
+    }
+    else{statusEl.textContent=game1.statusMessage()}
 })
--- hangman/hangman.js
+++ hangman/hangman.js
@@ -39,10 +39,26 @@ Hangman.prototype.getPuzzle=function(){
     })    
     return puzzle;
 }
+Hangman.prototype.statusMessage=function(){
+    if(this.staus==='FAILED')
+    {
+        return`FAILED THE WORD WAS ${game1.word.join('')}`
+    }
+    else if(this.staus==='PLAYING')
+    {
+        return `GUESSES LEFT --> ${game1.remainingGuesses}`
+    }
+    else{return'CONGRATULATIONS YOU HAVE GUESSED THE WORD'}
+}
 Hangman.prototype.makeguess=function(guess){
     guess=guess.toLowerCase()
     isUnique=!this.guessedletter.includes(guess)
     isBadGuess=!this.word.includes(guess)
+    if(!this.staus==='PLAYING')
+    {
+        return
+    }
+
     if(isUnique)
     {
     this.guessedletter.push(guess)}
