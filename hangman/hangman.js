c2f6e1048008945b46026d091d9d9dbc62bc8ce8
Nileshgarg13 <ng5787@srmist.edu.in>
2023-02-04 02:12:28 +0530

    converted to class

--- hangman/hangman.js
+++ hangman/hangman.js
@@ -6,29 +6,30 @@
 // Number: mynum -->Number.prototype--> object.prototype --> null
 // Boolean: mybool --> Boolean.prototype --> object.prototype --> null
 
-
-const Hangman = function (word,remainingguesses)
-{
+class Hangman{
+    constructor(word,remainingguesses)
+    {
     this.word=word.toLowerCase().split('')
     this.remainingGuesses=remainingguesses
     this.guessedletter=[]
     this.staus='PLAYING'
-} 
-Hangman.prototype.calculateStatus=function()
-{
-    if(game1.remainingGuesses===0)
-    {
-        this.staus='FAILED'
     }
-    else{
-    if(!game1.getPuzzle().includes('*'))
+    calculateStatus()
     {
-        this.staus='FINISHED'
-    }}
-}
-Hangman.prototype.getPuzzle=function(){
-    let puzzle=''
-    this.word.forEach((letter) => {
+        if(game1.remainingGuesses===0)
+        {
+            this.staus='FAILED'
+        }
+        else{
+        if(!game1.getPuzzle().includes('*'))
+        {
+            this.staus='FINISHED'
+        }}
+    }
+    getPuzzle()
+    {
+        let puzzle=''
+        this.word.forEach((letter) => {
         if(this.guessedletter.includes(letter)||letter===' '){
             puzzle+=letter
         }
@@ -38,22 +39,24 @@ Hangman.prototype.getPuzzle=function(){
         }
     })    
     return puzzle;
-}
-Hangman.prototype.statusMessage=function(){
-    if(this.staus==='FAILED')
-    {
-        return`FAILED THE WORD WAS ${game1.word.join('')}`
     }
-    else if(this.staus==='PLAYING')
+    statusMessage()
     {
-        return `GUESSES LEFT --> ${game1.remainingGuesses}`
+        if(this.staus==='FAILED')
+        {
+            return`FAILED THE WORD WAS ${game1.word.join('')}`
+        }
+        else if(this.staus==='PLAYING')
+        {
+            return `GUESSES LEFT --> ${game1.remainingGuesses}`
+        }
+        else{return'CONGRATULATIONS YOU HAVE GUESSED THE WORD'}
     }
-    else{return'CONGRATULATIONS YOU HAVE GUESSED THE WORD'}
-}
-Hangman.prototype.makeguess=function(guess){
+    makeguess(guess)
+    {
     guess=guess.toLowerCase()
-    isUnique=!this.guessedletter.includes(guess)
-    isBadGuess=!this.word.includes(guess)
+    const isUnique=!this.guessedletter.includes(guess)
+    const isBadGuess=!this.word.includes(guess)
     if(!this.staus==='PLAYING')
     {
         return
@@ -66,5 +69,69 @@ Hangman.prototype.makeguess=function(guess){
     {
         this.remainingGuesses--
     }
-    this.calculateStatus()
+    this.calculateStatus()   
+    }
+
 }
+
+// const Hangman = function (word,remainingguesses)
+// {
+//     this.word=word.toLowerCase().split('')
+//     this.remainingGuesses=remainingguesses
+//     this.guessedletter=[]
+//     this.staus='PLAYING'
+// } 
+// Hangman.prototype.calculateStatus=function()
+// {
+//     if(game1.remainingGuesses===0)
+//     {
+//         this.staus='FAILED'
+//     }
+//     else{
+//     if(!game1.getPuzzle().includes('*'))
+//     {
+//         this.staus='FINISHED'
+//     }}
+// }
+// Hangman.prototype.getPuzzle=function(){
+//     let puzzle=''
+//     this.word.forEach((letter) => {
+//         if(this.guessedletter.includes(letter)||letter===' '){
+//             puzzle+=letter
+//         }
+//         else
+//         {
+//             puzzle+='*'
+//         }
+//     })    
+//     return puzzle;
+// }
+// Hangman.prototype.statusMessage=function(){
+//     if(this.staus==='FAILED')
+//     {
+//         return`FAILED THE WORD WAS ${game1.word.join('')}`
+//     }
+//     else if(this.staus==='PLAYING')
+//     {
+//         return `GUESSES LEFT --> ${game1.remainingGuesses}`
+//     }
+//     else{return'CONGRATULATIONS YOU HAVE GUESSED THE WORD'}
+// }
+// Hangman.prototype.makeguess=function(guess){
+//     guess=guess.toLowerCase()
+//     isUnique=!this.guessedletter.includes(guess)
+//     isBadGuess=!this.word.includes(guess)
+//     if(!this.staus==='PLAYING')
+//     {
+//         return
+//     }
+
+//     if(isUnique)
+//     {
+//     this.guessedletter.push(guess)}
+//     if(isUnique && isBadGuess)
+//     {
+//         this.remainingGuesses--
+//     }
+//     this.calculateStatus()
+// }
